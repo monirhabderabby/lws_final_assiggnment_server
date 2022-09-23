6de1f8d82a803de00406f6eb0d081a99fa0a6f17
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-09-23 07:32:33 +0600

    Create server

--- server.js
+++ server.js
@@ -16,16 +16,6 @@ router.render = (req, res) => {
     const path = req.path;
     const method = req.method;
 
-    if (
-        path.includes("/conversations") &&
-        (method === "POST" || method === "PATCH")
-    ) {
-        // emit socket event
-        io.emit("conversation", {
-            data: res.locals.data,
-        });
-    }
-
     res.json(res.locals.data);
 };
 
@@ -39,11 +29,11 @@ app.use(middlewares);
 
 const rules = auth.rewriter({
     users: 640,
-    conversations: 660,
+    teams: 660,
     messages: 660,
 });
 
-app.use(rules);
+// app.use(rules);
 app.use(auth);
 app.use(router);
 
